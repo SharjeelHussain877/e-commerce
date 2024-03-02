c7f4ee41f74dfc8e2b8b57ff3f2fc213aaafdf5b
Sharjeel Hussain <sharjeelhussain877@gmail.com>
2024-03-03 02:22:39 +0500

    fix page css

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -18,19 +18,19 @@ function ContactCard() {
                 <Box mt={7} className='contact-info'>
                     <Typography mt={2} variant='body1'>
                         <WhatsAppIcon />
-                        <span>+92 3252105103</span>
+                        <a href='https://wa.me/+923252105103'>+92 3252105103</a>
                     </Typography>
                     <Typography mt={2} variant='body1'>
                         <MailOutlineIcon />
-                        <span>sharjeelhussain877@gmail.com</span>
+                        <a href='mailto:sharjeelhussain877@gmail.com'>sharjeelhussain877@gmail.com</a>
                     </Typography>
                     <Typography mt={2} variant='body1'>
                         <PinDropIcon />
-                        <span>
+                        <a href='https://www.google.com/maps?q=24.980749,66.9853283'>
                             75800 Gulshan-e-zia
                             Orangi Town, KHI
                             PK
-                        </span>
+                        </a>
                     </Typography>
                 </Box>
                 <Box mt={8} className='icons'>
